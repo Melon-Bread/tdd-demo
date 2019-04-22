39978a9570a0d1542c39d12f995b0483d44d3e0e
Melon Bread <rain@melonbread.dev>
2019-04-22 19:34:41 -0400

    Added some dummy commands to test subprocess

--- setup-everything.py
+++ setup-everything.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+import subprocess
 
 
 def main():
     print("hello world")
+    install_packages()
+
+
+def install_packages():
+    subprocess.call(['sudo', 'pacman', '-S', 'git'])
+    subprocess.call(['sudo', 'apt-get', 'install', 'python3-venv'])
 
 
 if __name__ == "__main__":
