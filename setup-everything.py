9c8a4a0bfe74e8bf6743ff97139ff84a95a3ccb0
Melon Bread <rain@melonbread.dev>
2019-04-24 00:58:28 -0400

    Got all three terminals to launch properly
    I need to go back and make the launch_terminals() method more pythonic

--- setup-everything.py
+++ setup-everything.py
@@ -6,6 +6,7 @@ import os
 def main():
     install_packages()
     setup_venv()
+    launch_terminals()
 
 
 def install_packages():
@@ -25,6 +26,18 @@ def setup_venv():
     print("Installing requrements")
     os.system('. env/bin/activate && pip install -r requirements.txt')
 
+
+def launch_terminals():
+    terminal_names = ['testing', 'git status', 'expore']
+    terminal_rcfiles = ['pytest-window-bashrc',
+                        'git-status-bashrc',
+                        'misc-bashrc']
+   
+    i = 0
+    while i < len(terminal_names):
+        subprocess.Popen(['xterm', '-T', terminal_names[i], 
+                         '-e', 'bash', '--rcfile', terminal_rcfiles[i]])
+        i += 1
 
 if __name__ == "__main__":
     main()
