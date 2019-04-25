1373d159e8dee24ef5ebde9b1bb2705e414edaeb
Melon Bread <rain@melonbread.dev>
2019-04-25 17:36:24 -0400

    In progress replacing raw terminal call with Popen

--- setup-everything.py
+++ setup-everything.py
@@ -1,17 +1,20 @@
-#!/usr/bin/env python3
+In progre#!/usr/bin/env python3
 import subprocess
 import os
 import platform
 import sys
 
+OPERATING_SYSTEM = platform.system()
+
+
 def main():
     install_packages()
     setup_venv()
-    launch_terminals(platform.system())
+    launch_terminals()
 
 
-def install_packages(operating_system):
-    if operating_system == 'Linux':
+def install_packages():
+    if OPERATING_SYSTEM == 'Linux':
         packages = ['python3-venv', 'meld', 'xterm']
 
         subprocess.call(['sudo', 'apt-get', 'update'])
@@ -37,13 +40,15 @@ def launch_terminals():
     terminal_rcfiles = ['pytest-window-bashrc',
                         'git-status-bashrc',
                         'misc-bashrc']
-   
+
     i = 0
     while i < len(terminal_names):
-        subprocess.Popen(['xterm', '-T', terminal_names[i], 
-                         '-e', 'bash', '--rcfile', terminal_rcfiles[i]])
+        #subprocess.Popen(['xterm', '-T', terminal_names[i],
+        #                  '-e', 'bash', '--rcfile', terminal_rcfiles[i]])
+        subprocess.Popen(['bash', '--rcfile', terminal_rcfiles[i]],
+                         creationflags=subprocess.CREATE_NEW_CONSOLE, shell=False)
         i += 1
-            
+
 
 if __name__ == "__main__":
     main()
