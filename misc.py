e9c6d898bfd19989d97601c35923d46b7f324bf7
Melon Bread <rain@melonbread.dev>
2019-05-16 17:00:14 -0400

    Making things harder than needed

--- misc.py
+++ misc.py
@@ -4,11 +4,7 @@ import platform
 import os
 
 for i in range(6):
-    if i == 4 or 6:
-        result = 'pass'
-    else:
-        result = 'fail'
-    print('git checkout step-{} watch tests {} in py.test window'.format(i, result))
+    print('git checkout step-{}'.format(i))
 
 if platform.system == 'Windows':
     os.system('env\\Scripts\\activate.bat')
